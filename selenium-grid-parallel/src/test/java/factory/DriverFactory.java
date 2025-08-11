3ff6dad884284e816887ded084d2bfdb53b6338b
venkiralami <venki.ralami@gmail.com>
2025-08-11 09:49:14 -0500

    New Selenium Grid project

---
+++ selenium-grid-parallel/src/test/java/factory/DriverFactory.java
@@ -0,0 +1,18 @@
+package factory;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import java.net.URL;
+
+public class DriverFactory {
+    public static WebDriver createDriver(String browser) throws Exception {
+        URL gridUrl = new URL("http://localhost:4444/wd/hub");
+        if (browser.equalsIgnoreCase("chrome")) {
+            return new RemoteWebDriver(gridUrl, new ChromeOptions());
+        } else if (browser.equalsIgnoreCase("firefox")) {
+            return new RemoteWebDriver(gridUrl, new FirefoxOptions());
+        }
+        throw new IllegalArgumentException("Unsupported browser:: " + browser);
+    }
+}
---
+++ selenium-grid-parallel/src/test/java/tests/GridTest.java
@@ -0,0 +1,85 @@
+package tests;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+import factory.DriverFactory;
+
+public class GridTest {
+    WebDriver driver;
+    static ExtentReports extent;
+    ExtentTest test;
+
+    @BeforeSuite(alwaysRun = true)
+    public void setupExtent() {
+        if (extent == null) {
+            ExtentSparkReporter reporter = new ExtentSparkReporter("target/GridParallelReport.html");
+            extent = new ExtentReports();
+            extent.attachReporter(reporter);
+        }
+    }
+    
+    @AfterSuite(alwaysRun = true)
+    public void tearDownExtent() {
+        if (extent != null) {
+            extent.flush();
+        }
+    }
+    
+    @Parameters("browser")
+    @BeforeMethod(alwaysRun = true)
+    public void setup(String browser) throws Exception {
+        test = extent.createTest("Test on :: " + browser);
+        driver = DriverFactory.createDriver(browser);
+    }
+
+    @Test
+    public void openGoogle() {
+        driver.get("https://www.google.com");
+        test.pass("Page title:: " + driver.getTitle());
+    }
+
+    @AfterMethod
+    public void teardown(ITestResult result) {
+        if (result.getStatus() == ITestResult.FAILURE) {
+            String screenshotPath = takeScreenshot(result.getName());
+            try {
+                test.fail(result.getThrowable()).addScreenCaptureFromPath(screenshotPath);
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
+        }
+        if (driver != null) {
+           // driver.quit();
+        }
+    }
+
+    public String takeScreenshot(String testName) {
+        String dateName = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+        File source = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        String destination = System.getProperty("user.dir") + "/screenshots/" + testName + "_" + dateName + ".png";
+        try {
+            FileUtils.copyFile(source, new File(destination));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return destination;
+    }
+}
